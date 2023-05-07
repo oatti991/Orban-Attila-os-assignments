6742abe14873f56952ec85f284ebecf1f5f80e13
Orban <orban.va.attila@student.utcluj.ro>
2023-05-07 22:54:02 +0300

    2.4,2.5 partial

--- a2/a2.c
+++ a2/a2.c
@@ -40,14 +40,45 @@ void *t_func(void *arg)
     {
         info(BEGIN, s->process_no, id);
         info(END, s->process_no, id);
+
     }
     pthread_exit(NULL);
+}
+
+void* t_func2(void *arg)
+{
+
+    TH_STRUCT *s = (TH_STRUCT *)arg;
+    int id = s->thread_no;
+    sem_wait(&s1);
+    info(BEGIN, s->process_no, id);
+    info(END, s->process_no, id);
+    sem_post(&s1);
+    pthread_exit(NULL);
+}
+
+void* t_func3(void *arg)
+{
+    TH_STRUCT *s = (TH_STRUCT *)arg;
+    int id = s->thread_no;
+
+    info(BEGIN, s->process_no, id);
+    info(END, s->process_no, id);
+
+    pthread_exit(NULL);
+
 }
 
 int main(int argc, char **argv)
 {
     pthread_t arr[4];
     TH_STRUCT no[5];
+
+    pthread_t arr2[40];
+    TH_STRUCT no2[41];
+
+    pthread_t arr3[6];
+    TH_STRUCT no3[7];
     pid_t p2, p3, p4, p5, p6, p7, p8, p9;
     init();
     info(BEGIN, 1, 0);
@@ -56,10 +87,23 @@ int main(int argc, char **argv)
     if (p2 == 0)
     {
         info(BEGIN, 2, 0);
+
+
+
         p3 = fork();
         if (p3 == 0)
         {
             info(BEGIN, 3, 0);
+            for (int i = 0; i < 6; i++)
+            {
+                no3[i].process_no = 3;
+                no3[i].thread_no = i + 1;
+                pthread_create(&arr3[i], NULL, t_func3, &no3[i]);
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                pthread_join(arr3[i], NULL);
+            }
             p4 = fork();
             if (p4 == 0)
             {
@@ -121,6 +165,19 @@ int main(int argc, char **argv)
         }
         waitpid(p3, NULL, 0);
         waitpid(p5, NULL, 0);
+
+        sem_init(&s1, 0, 6);
+        for (int i = 0; i < 40; i++)
+        {
+            no2[i].process_no = 2;
+            no2[i].thread_no = i + 1;
+            pthread_create(&arr2[i], NULL, t_func2, &no2[i]);
+        }
+        for (int i = 0; i < 40; i++)
+        {
+            pthread_join(arr2[i], NULL);
+        }
+        sem_destroy(&s1);
         info(END, 2, 0);
         exit(0);
     }
